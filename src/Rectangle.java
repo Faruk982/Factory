061ed148a7951feb8e6a84726d98b06eda77b389
faruk <faruk2007016@stud.kuet.ac.bd>
2024-01-24 09:17:18 +0600

    second second commit

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,7 +1,16 @@
 public class Rectangle implements Shape {
     @Override
-
     public void draw() {
         System.out.println("Inside Rectangle::draw() method.");
     }
+    private static Rectangle ins = null;
+    private Rectangle(){}
+    public static Rectangle getInstance()
+    {
+        if (ins == null)
+            ins = new Rectangle();
+        return ins;
+    }
+
+
 }
