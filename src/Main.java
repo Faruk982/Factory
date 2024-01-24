4a41cae8ebd15d32e2ca83608847d1e65568e644
faruk <faruk2007016@stud.kuet.ac.bd>
2024-01-24 10:00:19 +0600

    last one

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ public class Main {
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        ShapeFactory shapeFactory = new ShapeFactory();
-        Shape shape1 = shapeFactory.getShape("Circle");
+        ShapeFactory shapeFactory = ShapeFactory.getInstance();
+        ShapeFactory shapeFactory2 =  ShapeFactory.getInstance();
+
+        Shape shape1 = shapeFactory.getShape("Square");
         if(shape1!=null)shape1.draw();
         Shape shape2 = shapeFactory.getShape("RECTANGLE");
         if(shape2!=null)shape2.draw();
+        System.out.println("Hashcode of 1 is "
+                + shapeFactory.hashCode());
+        System.out.println("Hashcode of 2 is "
+                + shapeFactory2.hashCode());
         //System.out.printf("Hello and welcome!");
     }
 }
